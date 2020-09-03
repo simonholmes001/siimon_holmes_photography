10c9d147f74913b1ddf72cc89012d8fff3baa39d
simonholmes001 <simonholmesabc@gmail.com>
2020-09-03 13:04:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+## TO DO
+
+### Branch: navbar
+- [ ] Decide on navbar headers
+
+### Branch: landing page
+- [ ] 
+- [ ] 
+- [ ]  
+- [ ] 
+- [ ] 
+- [ ] 
+
+### Branch: recipes
+- [ ] 
+- [ ] 
+- [ ] 
+- [ ] 
+- [ ] 
+- [ ] 
+- [ ] 
+
+### Branch: footer
+- [ ] Add in a copyright 
+- [ ] Add in the link to github
+
+### General
+- [ ] Find out how to host the webpage (netify or google)
+- [ ] Find out how to transfer domain to netify for hosting if necessary / requested
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"gopkg.in/yaml.v2"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("./templates/*.html"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	// http.HandleFunc("/ratatouille", ratatouille)
+	// The following two lines of code allows go to serve the static asset files
+	fs := http.FileServer(http.Dir("assets/"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	// Listen and Serve
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	var test config
+	yamlFile, err := ioutil.ReadFile("./assets/structures/config.yaml")
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, &test)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+	tpl.ExecuteTemplate(w, "index.html", test)
+}
+
+// THIS WAS THE LAST ONE WORKING
+type config struct {
+	PageTitle string   `yaml:"pagetitle"`
+	Navbar    []string `yaml:"navbar"`
+	Brand     string   `yaml:"brand"`
+	Intro     struct {
+		Title1 string   `yaml:"title1"`
+		Title2 string   `yaml:"title2"`
+		Title3 []string `yaml:"title3"`
+	} `yaml:"intro"`
+	RecipeList []string `yaml:"recipe_list"`
+	NewRecipes []struct {
+		Recipe            string   `yaml:"recipe"`
+		PreparationTime   string   `yaml:"preparation_time"`
+		CookingTime       string   `yaml:"cooking_time"`
+		Difficulty        string   `yaml:"difficulty"`
+		Ingredients       []string `yaml:"ingredients"`
+		Method            string   `yaml:"method"`
+		Tags              []string `yaml:"tags"`
+		PhotoURL          string   `yaml:"photo_url"`
+		VideoURL          string   `yaml:"video_url"`
+		RatatouilleEnable bool     `yaml:"ratatouille_enable"`
+		Homo2Enable       bool     `yaml:"homo2_enable"`
+		Homo3Enable       bool     `yaml:"homo3_enable"`
+	} `yaml:"new_recipes"`
+}
+
+// other templates that are called b y the main.go script
+
+func ratatouille(w http.ResponseWriter, r *http.Request) {
+	var test Intro
+	yamlFile, err := ioutil.ReadFile("./assets/structures/homepage.yaml")
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, &test)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+	tpl.ExecuteTemplate(w, "ratatouille.html", test)
+}
